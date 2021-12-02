59196fc151cdaff48abdcd979530351fed3ae7c4
Alessio Tiberi <alessio.1997.18.01@gmail.com>
2021-12-02 16:09:15 +0100

    finish

---
+++ myscript.js
@@ -0,0 +1,13 @@
+// NAME 
+
+let userInputName = prompt("Inserisci nome");
+
+// COGNOME 
+
+let userInputLastName = prompt("Inserisci cognome");
+
+// COLORE 
+
+let userInputColor = prompt("Iserisci il tuo colore preferito");
+
+document.getElementById("bruce").innerHTML = userInputName + userInputLastName + userInputColor;
